b3ffa77bed9f264afc3768c5633ad47c5e21a226
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-24 17:47:38 +0800

    Add hero component

---
+++ src/components/hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="text-gray-700 body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900 leading-10 sm:leading-normal">
+            Hi! 👋🏼 I&apos;m Muhammad Fawwaz,&nbsp;
+            <br className="hidden lg:inline-block" />
+            a full stack 🍔 developer&nbsp;
+            <br className="hidden lg:inline-block" />
+            with experience in&nbsp;
+            <br className="hidden lg:inline-block" />
+            tech 🔧 and finance 💸
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
