83b9729c4f49d56fd3d287b1b6348538b2cddb19
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-11 12:49:13 +0800

    Add dark state and fix position of dark switcher

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Header() {
+  const [dark, setDark] = useState(
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+  useEffect(() => {
+    if (dark) {
+      !document.body.classList.contains("dark") && document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.contains("dark") &&
+        document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [dark]);
   return (
-    <header className="h-10">
+    <header className="h-10 fixed right-3 top-3">
+      <button
+        className="mode outline-none focus:outline-none"
+        onClick={() => setDark((dark) => !dark)}
+      ></button>
     </header>
   );
 }
